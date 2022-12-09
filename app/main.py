f8762aeed346f5781cbb6422cf3ed0ad5fe5114d
ABHISHEK KUMAR VERMA <vermaabhishek357@gmail.com>
2022-12-09 14:32:50 +0100

    modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -26,3 +26,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
