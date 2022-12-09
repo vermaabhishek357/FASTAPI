bd3ea334a7eb231888bf1166285de0578a7b0a67
ABHISHEK KUMAR VERMA <vermaabhishek357@gmail.com>
2022-12-09 12:49:25 +0100

    added codes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from . import models
 from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
-
+from fastapi.middleware.cors import CORSMiddleware
 print(settings.database_username)
 
 #call create engine to create tables from models using sqlalchemy
@@ -11,6 +11,16 @@ print(settings.database_username)
 
 # initiate the app
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
